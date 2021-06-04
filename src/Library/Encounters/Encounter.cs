9b5a13a08e9125355868bf84bff2f787a99a6674
jandresleal <62621149+jandresleal@users.noreply.github.com>
2021-06-03 21:23:59 -0300

    implementacion de battlefield

---
+++ src/Library/Encounters/Encounter.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+namespace RoleplayGame
+{
+    public abstract class Encounter
+    {
+        protected List<Heros> herolist = new List<Heros>();
+        protected List<BadGuys> badguylist = new List<BadGuys>();
+
+        public abstract void DoEncounter()
+        {
+            
+        }
+    }
+}
